64222372ad80a5cc3d6ab219ddedbe6e098034d4
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2024-12-18 15:51:26 +0000

    add components folder for data factory

---
+++ components/main.tf
@@ -0,0 +1,17 @@
+module "datafactory" {
+  source = "git@github.com:hmcts/terraform-module-azure-datafactory?ref=main"
+  env    = var.env
+
+  product   = "baubais"
+  component = "datafactory"
+
+  common_tags = module.common_tags.common_tags
+}
+
+module "common_tags" {
+  source = "github.com/hmcts/terraform-module-common-tags?ref=master"
+
+  builtFrom   = "hmcts/terraform-module-mssql"
+  environment = var.env
+  product     = "sds-platform"
+}
---
+++ components/providers.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "74dacd4f-a248-45bb-a2f0-af700dc4cf68"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "74dacd4f-a248-45bb-a2f0-af700dc4cf68"
+  alias           = "juror-er"
+}

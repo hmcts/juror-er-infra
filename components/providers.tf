d749c2553d568c58fc81c765409d017d3cc77447
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-12-19 13:47:52 +0000

    adding sp vars into pipeline for auth

--- components/providers.tf
+++ components/providers.tf
@@ -16,5 +16,7 @@ terraform {
     storage_account_name = "stojurorer"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    subscription_id      = "74dacd4f-a248-45bb-a2f0-af700dc4cf68"
+    tenant_id            = "531ff96d-0ae9-462a-8d2d-bec7c0b42082"
   }
 }

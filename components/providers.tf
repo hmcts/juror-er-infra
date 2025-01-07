3ceb6fb85a161f315da605bd4f2be0862d75c052
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2025-01-07 18:15:15 +0000

    test pipeline

--- components/providers.tf
+++ components/providers.tf
@@ -16,5 +16,6 @@ terraform {
     storage_account_name = "c74dacd4faf700dc4cf68sa"
     container_name       = "subscription-tfstate"
     key                  = "UK South/juror-er-infra/terraform.tfstate"
+    subscription_id      = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
   }
 }

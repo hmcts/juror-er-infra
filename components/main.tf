f1eb1b19dd2c564e1985be1fd58256389d04707d
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2025-01-07 12:33:30 +0000

    create storage account using hmcts control subs

--- components/main.tf
+++ components/main.tf
@@ -22,3 +22,12 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = module.tags.common_tags
 }
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS"
+  tags                     = module.tags.common_tags
+}

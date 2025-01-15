5f5556be1b9932e5e422c0783aa8a94b23fd71a7
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2025-01-15 10:35:04 +0000

    add storage account in stg

--- components/data-factory/main.tf
+++ components/data-factory/main.tf
@@ -15,3 +15,18 @@ module "tags" {
   environment = var.env
   product     = "sds-platform"
 }
+
+resource "azurerm_storage_account" "adf_jurerstg_" {
+  name                            = var.storage_account_name
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_tier                    = var.storage_account_tier
+  account_replication_type        = var.storage_account_replication
+  account_kind                    = var.storage_account_type
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled   = false
+  tags                            = module.ctags.common_tags
+  lifecycle {
+    prevent_destroy = false
+  }
+}

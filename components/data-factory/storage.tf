d34e908c7c28ecacdecdb92e30c1cbfe656d233b
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2025-01-15 11:43:47 +0000

    move storage account to it's own file

---
+++ components/data-factory/storage.tf
@@ -0,0 +1,14 @@
+resource "azurerm_storage_account" "adf_jurerstg" {
+  name                            = "baubaisadfsa${var.env}"
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_tier                    = var.storage_account_tier
+  account_replication_type        = var.storage_account_replication
+  account_kind                    = var.storage_account_type
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled   = false
+  tags                            = module.tags.common_tags
+  lifecycle {
+    prevent_destroy = false
+  }
+}

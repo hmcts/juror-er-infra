a84c4e63aaeb3be04aa6b7ff81a6c37fe50c365a
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-04-25 10:39:47 +0100

    added config for resource group

--- components/data-factory/init.tf
+++ components/data-factory/init.tf
@@ -22,3 +22,14 @@ provider "azurerm" {
   features {}
   subscription_id = var.hub_subscription_id
 }
+
+resource "azurerm_resource_group" "adf_juror_rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = module.tags.common_tags
+}
+
+import {
+  to = azurerm_resource_group.adf_juror_rg
+  id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/baubais-data-factory-rg-stg"
+}

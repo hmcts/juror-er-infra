797511aa71b5823e9bb843071fbb8d7bb3da938c
qzhou <qiang.zhou@hmcts.net>
2025-10-28 16:25:41 +0000

    Revert kv changes

--- components/data-factory/keyvault.tf
+++ components/data-factory/keyvault.tf
@@ -8,54 +8,46 @@ resource "azurerm_key_vault" "baubais_kv" {
   tenant_id                = data.azurerm_client_config.current.tenant_id
   purge_protection_enabled = false
 
-  tags = module.tags.common_tags
-}
+  access_policy = [
+    {
+      tenant_id      = data.azurerm_client_config.current.tenant_id
+      object_id      = data.azurerm_client_config.current.object_id
+      application_id = null
 
-resource "azurerm_key_vault_access_policy" "keyvault_ado_access_policy" {
-  key_vault_id   = azurerm_key_vault.baubais_kv.id
-  object_id      = data.azurerm_client_config.current.object_id
-  tenant_id      = data.azurerm_client_config.current.tenant_id
-  application_id = null
+      certificate_permissions = []
+      key_permissions         = []
+      storage_permissions     = []
 
-  certificate_permissions = []
-  key_permissions         = []
-  storage_permissions     = []
-
-  secret_permissions = [
+      secret_permissions = [
         "Get",
         "List",
         "Set",
         "Delete",
         "Purge"
       ]
-}
+      }, {
+      tenant_id      = data.azurerm_client_config.current.tenant_id
+      object_id      = var.bootstrap_object_id
+      application_id = null
 
-resource "azurerm_key_vault_access_policy" "keyvault_bootstrap_access_policy" {
-  key_vault_id   = azurerm_key_vault.baubais_kv.id
-  object_id      = var.bootstrap_object_id
-  tenant_id      = data.azurerm_client_config.current.tenant_id
-  application_id = null
+      certificate_permissions = []
+      key_permissions         = []
+      storage_permissions     = []
 
-  certificate_permissions = []
-  key_permissions         = []
-  storage_permissions     = []
-
-  secret_permissions = [
+      secret_permissions = [
         "Get",
         "List",
         "Set",
         "Delete",
         "Purge"
       ]
-}
+    },
+    {
+      tenant_id      = data.azurerm_client_config.current.tenant_id
+      object_id      = "e7ea2042-4ced-45dd-8ae3-e051c6551789" # DTS Platform Operations
+      application_id = null
 
-resource "azurerm_key_vault_access_policy" "keyvault_dts_operation_access_policy" {
-  key_vault_id   = azurerm_key_vault.baubais_kv.id
-  object_id      = "e7ea2042-4ced-45dd-8ae3-e051c6551789" # DTS Platform Operations
-  tenant_id      = data.azurerm_client_config.current.tenant_id
-  application_id = null
-
-  certificate_permissions = [
+      certificate_permissions = [
         "Get",
         "List",
         "Update",
@@ -95,4 +87,8 @@ resource "azurerm_key_vault_access_policy" "keyvault_dts_operation_access_policy
         "Delete",
         "Purge"
       ]
+    }
+  ]
+
+  tags = module.tags.common_tags
 }
